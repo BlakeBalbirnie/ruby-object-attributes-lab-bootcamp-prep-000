56971d0e36eaff2c164509aed415d7031530817b
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-11-22 16:19:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,16 +4,16 @@ class Dog
     @name
   end
 
-  def name=(dog_name)
-    @name = dog_name
+  def name=(new_name)
+    @name = new_name
   end
 
   def breed
     @breed
   end
 
-  def breed=(dog_breed)
-    @breed = dog_breed
+  def breed=(new_breed)
+    @breed = new_breed
   end
 
 end
